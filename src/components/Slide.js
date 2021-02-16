534ad48a721b73b8cbe241f223a70530cef13366
Mizan <mizan.softdev@gmail.com>
2021-02-16 23:44:40 +0600

    react slider

---
+++ src/components/Slide.js
@@ -0,0 +1,20 @@
+import React, { memo } from 'react'
+// import { css, jsx } from '@emotion/react'
+import { css } from '@emotion/css'
+
+const Slide = ({ content, width }) => {
+  return (
+    <div
+      className={css`
+        height: 100%;
+        width: ${width}px;
+        background-image: url('${content}');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+      `}
+    />
+  )
+}
+
+export default memo(Slide)
